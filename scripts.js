75674289e06243bb9abd10c83cae2cb13634369f
Anya Snow <emailasnow@gmail.com>
2019-03-08 02:28:34 -0800

    add weather projection functionality

--- scripts.js
+++ scripts.js
@@ -41,14 +41,33 @@ function generateWeather(city) {
 		throw new Error(response.statusText)
  	})
 	.then(responseJsonWeather => {
-	renderWeather(responseJsonWeather)})
+		generateHistory(responseJsonWeather)
+		renderWeather(responseJsonWeather)})
 	.catch(error => {
 		$('#error-message').html(`something went wrong: ${error.message}`)
 	})
  }
 
- function generateRecs(city) {
+function generateHistory(responseJsonWeather) {
+	const month = $('#travelmonth').val();
+	const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/dc1ac9820b3586324209af8c5002d883/${responseJsonWeather.coord.lat},${responseJsonWeather.coord.lon},2019-${month}-10T12:00:00`
+	fetch(url)
+		.then(response => {
+			if (response.ok) {
+				return response.json();
+			}
+			throw new Error(response.statusText)
+		})
+		.then(responseJsonHistory => {
+			console.log(responseJsonHistory)
+			renderHistory(responseJsonHistory)
+		})
+		.catch(error => {
+			$('#error-message').html(`something went wrong: ${error.message}`)
+		})
+}
 
+ function generateRecs(city) {
   	const url = `https://api.foursquare.com/v2/venues/explore?near=${city}&client_id=G0ZTRLEUNDR53BOKWQUKUOKJHNBVACEMHJRGGHDAN2HYAQRH&client_secret=DNNFDZOEDGOAAHLAUHRMZMZOFAKCT5BZY5RE13VHS5JATSXB&v=20190301`
 	fetch(url)
 	.then(response => {
@@ -73,8 +92,17 @@ function renderPics(responseJson) {
 }
 
 function renderWeather(responseJsonWeather) {
-  const weather = `<h2>Current Weather Conditions:</h2><div id="openweathermap-widget-19"></div> <script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = []; window.myWidgetParam.push({id: 19,cityid: '${responseJsonWeather.id}',appid: '3a891b3d82936c6e090048a99d733621',units: 'imperial',containerid: 'openweathermap-widget-19', }); (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s); })();</script>;` 
-  $(".weather").html(weather);
+  const weather = `</h2><div id="openweathermap-widget-19"></div> <script>window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = []; window.myWidgetParam.push({id: 19,cityid: '${responseJsonWeather.id}',appid: '3a891b3d82936c6e090048a99d733621',units: 'imperial',containerid: 'openweathermap-widget-19', }); (function() {var script = document.createElement('script');script.async = true;script.charset = "utf-8";script.src = "//openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s); })();</script>` 
+  $(".weather").html(weather)
+}
+
+function renderHistory(responseJsonHistory) {
+	const month = $('#travelmonth').val();
+	let history = responseJsonHistory.daily.data.map(element => {
+		return `<h3> The projected weather for ${month}/2019 based on historical averages is a high of ${element.temperatureHigh.toFixed(0)}&#176;F and a low of ${element.temperatureLow.toFixed(0)}&#176;F ${element.summary}</h3>`;
+	});
+	$(".history").html(history)
+	console.log(history);
 }
 
 function renderRecs(responseJsonRecs, city) {
