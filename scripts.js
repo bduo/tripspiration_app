d7bf3115828a644598b78752059c5c3cddded96f
Brandon El Haj <brandon.elhaj@gmail.com>
2019-03-09 16:04:30 -0800

    Latest Trip Version

--- scripts.js
+++ scripts.js
@@ -1,47 +1,47 @@
 "use strict";
 
 function submitForm() {
-  	$(".form").submit(function(event) {
-    event.preventDefault();
+	$(".form").submit(function(event) {
+		event.preventDefault();
 		$("h2").removeClass("hidden");
 		const userInput = $("#searchbox").val();
-    generateCityPics(userInput);
-    generateWeather(userInput);
-    generateRecs(userInput)
-  	});
+		generateCityPics(userInput);
+		generateWeather(userInput);
+		generateRecs(userInput)
+	});
 }
 
 function noFocus() {
 	$(".form").on(click, (event) => {
-	event.preventDefault();
-	$("#travelmonth:focus, #submit:focus, #searchbox:focus").css("outline", "none");
+		event.preventDefault();
+		$("#travelmonth:focus, #submit:focus, #searchbox:focus").css("outline", "none");
 	})
 }
 
 function generateCityPics(city) {
-		const url = `https://pixabay.com/api/?key=11800701-4991cb3dddbd2f2db0ae8b7de&q=${city}&image_type=photo&orientation=horizontal&per_page=8`
-  	fetch(url)
-  	.then(response => {
+	const url = `https://pixabay.com/api/?key=11800701-4991cb3dddbd2f2db0ae8b7de&q=${city}&image_type=photo&orientation=horizontal&per_page=8`
+	fetch(url)
+	.then(response => {
 		if (response.ok) {
 			return response.json();
 		}
 		throw new Error(response.statusText)
 	})
-  	.then(responseJson => {
+	.then(responseJson => {
 		if (responseJson.hits.length===0) {
-		const badResults = '<p class="alert-text">Sorry no photos for this city yet, please check your spelling or try a different location.</p>';
+			const badResults = '<p class="alert-text">Sorry no photos for this city yet, please check your spelling or try a different location.</p>';
 			$(".gallery").html(badResults);
 		} else return renderPics(responseJson);
-  	})
-  	.catch(error => {
-      $('#error-message-pics').html(`Sorry couldn't find the pictures, check spelling: ${error.message}`)
-  	})
- }
+	})
+	.catch(error => {
+		$('#error-message-pics').html(`Sorry couldn't find the pictures, check spelling: ${error.message}`)
+	})
+}
 
 function generateWeather(city) {
-  	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3a891b3d82936c6e090048a99d733621&units=imperial`
-  	fetch(url)
-  	.then(response => {
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=3a891b3d82936c6e090048a99d733621&units=imperial`
+	fetch(url)
+	.then(response => {
 		if (response.ok) {
 		return response.json();
 		}
@@ -59,28 +59,28 @@ function generateHistory(responseJsonWeather) {
 	const month = $('#travelmonth').val();
 	const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/dc1ac9820b3586324209af8c5002d883/${responseJsonWeather.coord.lat},${responseJsonWeather.coord.lon},2019-${month}-10T12:00:00`
 	fetch(url)
-		.then(response => {
-			if (response.ok) {
-				return response.json();
-			}
-			throw new Error(response.statusText)
-		})
-		.then(responseJsonHistory => {
-			renderHistory(responseJsonHistory)
-		})
-		.catch(error => {
-			$('#error-message-history').html(`Sorry couldn't find historical weather date, check spelling: ${error.message}`)
-		})
+	.then(response => {
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error(response.statusText)
+	})
+	.then(responseJsonHistory => {
+		renderHistory(responseJsonHistory)
+	})
+	.catch(error => {
+		$('#error-message-history').html(`Sorry couldn't find historical weather date, check spelling: ${error.message}`)
+	})
 }
 
  function generateRecs(city) {
-  	const url = `https://api.foursquare.com/v2/venues/explore?near=${city}&client_id=G0ZTRLEUNDR53BOKWQUKUOKJHNBVACEMHJRGGHDAN2HYAQRH&client_secret=DNNFDZOEDGOAAHLAUHRMZMZOFAKCT5BZY5RE13VHS5JATSXB&v=20190301`
+  const url = `https://api.foursquare.com/v2/venues/explore?near=${city}&client_id=G0ZTRLEUNDR53BOKWQUKUOKJHNBVACEMHJRGGHDAN2HYAQRH&client_secret=DNNFDZOEDGOAAHLAUHRMZMZOFAKCT5BZY5RE13VHS5JATSXB&v=20190301`
 	fetch(url)
 	.then(response => {
-	if (response.ok) {
-		return response.json();
-	}
-	throw new Error(response.statusText)
+		if (response.ok) {
+			return response.json();
+		}
+		throw new Error(response.statusText)
 	})
 	.then(responseJsonRecs => {
 		renderRecs(responseJsonRecs, city)})
@@ -90,11 +90,11 @@ function generateHistory(responseJsonWeather) {
 }
 
 function renderPics(responseJson, city) {
-  	const results = responseJson.hits.map(element => {
-		return `
-   <a href="${element.pageURL}" target="_blank"><img src="${element.webformatURL}" alt="${element.tags}" class="results-img" />`;});
-  	$(".gallery").html(results);
-  	$('#searchbox').val("");
+	const results = responseJson.hits.map(element => {
+	return `
+		<a href="${element.pageURL}" target="_blank"><img src="${element.webformatURL}" alt="${element.tags}" class="results-img" />`;});
+	$(".gallery").html(results);
+	$('#searchbox').val("");
 }
 
 function renderWeather(responseJsonWeather) {
@@ -113,11 +113,11 @@ function renderHistory(responseJsonHistory) {
 function renderRecs(responseJsonRecs, city) {
   let recs = responseJsonRecs.response.groups.map( element => {
 		return element.items.map(ele => {
-		return `<h3 class="button-closer">${ele.venue.name}</h3>
-		<a href="https://www.google.com/search?q=${ele.venue.name}+${city}" target="_blank" class="own-line"><i class="fas fa-globe-americas"></i>&nbsp;more info: Google &nbsp;</a>`
-	}).join("")
-});
-    $(".recs").html(recs);
+			return `<h3 class="button-closer">${ele.venue.name}</h3>
+			<a href="https://www.google.com/search?q=${ele.venue.name}+${city}" target="_blank" class="own-line"><i class="fas fa-globe-americas"></i>&nbsp;more info: Google &nbsp;</a>`
+		}).join("")
+	});
+  $(".recs").html(recs);
 }
 
 $(submitForm);
